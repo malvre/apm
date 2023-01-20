2a0fb9895b501bcfb3aba286addabfdbc28ce90e
Marcelo Rezende <malvre@gmail.com>
2023-01-20 08:35:37 -0300

    user detail using offcanvas

--- src/app/users/pages/user-list/user-list.component.ts
+++ src/app/users/pages/user-list/user-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user-list',
@@ -12,8 +13,11 @@ import { RouterLink } from '@angular/router';
 })
 export class UserListComponent implements OnInit {
   users: any[] = [];
+  activeUser: any = {};
 
   userService = inject(UserService);
+  router = inject(Router);
+  offcanvasService = inject(NgbOffcanvas);
 
   ngOnInit(): void {
     this.loadData();
@@ -23,5 +27,18 @@ export class UserListComponent implements OnInit {
     this.userService.all().subscribe((response) => {
       this.users = response.data;
     });
+  }
+
+  onEdit(event: any, user: any) {
+    event.stopPropagation();
+    this.router.navigateByUrl(`/users/${user.id}/edit`);
+  }
+
+  onDetail(content: any, user: any) {
+    this.activeUser = user;
+    this.offcanvasService.open(content, { position: 'end' }).result.then(
+      (result) => {},
+      (reason) => {}
+    );
   }
 }
--- src/app/users/pages/user.routes.ts
+++ src/app/users/pages/user.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 import { UserListComponent } from './user-list/user-list.component';
 
 export const userRoutes: Routes = [
@@ -14,5 +15,16 @@ export const userRoutes: Routes = [
     title: 'User detail',
     pathMatch: 'full',
     component: UserDetailComponent,
+  },
+  {
+    path: ':id/edit',
+    title: 'User edit',
+    pathMatch: 'full',
+    component: UserEditComponent,
+  },
+  {
+    path: 'add',
+    title: 'New user',
+    component: UserEditComponent,
   },
 ];
